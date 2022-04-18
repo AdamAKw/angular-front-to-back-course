035e810accd5a0cae8d4575edf66a6bcfdf24922
AdamAKw <kwiatkowskiadam1ta@gmail.com>
2022-04-18 17:43:53 +0200

    component chapter ends

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { UserComponent} from './components/user/user.component'
+import { UserComponent} from './components/user/user.component';
+import { UsersComponent } from './components/users/users.component'
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule
--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,7 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {User} from "../../models/User";
 
 @Component({
   selector: 'app-user',
-  templateUrl: './user.component.html'
+  templateUrl: 'user.component.html',
+  styleUrls: ['user.component.css']
+  // styles:[`
+  // .red {
+  //   color: red;
+  // }`]
 })
-export class UserComponent {}
+export class UserComponent implements OnInit{
+  user: User;
+
+  constructor() {
+    console.log('constructor');
+    this.user = {
+      firstName: 'John',
+      lastName: 'Doe',
+      age: 30,
+      address: {
+        street: '50 Main st',
+        city: 'Boston',
+        state: 'MA'
+      }
+    }
+  }
+
+  getAge(): number {
+    return this.user.age;
+  }
+
+  ngOnInit(): void {
+    console.log('do something after initialize')
+  }
+}
