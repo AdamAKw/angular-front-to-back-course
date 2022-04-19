18930c9372b81d675ec77cb5987226a87a65db81
AdamAKw <kwiatkowskiadam1ta@gmail.com>
2022-04-19 21:13:24 +0200

    chapter 4 end

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {User} from '../../models/User';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +7,75 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  users: User[];
+  showExtended: boolean = false;
+  currentClasses = {};
+  currentStyles = {};
+  isActive: boolean = true;
 
-  constructor() { }
+  constructor() {
+    this.users = [{
+      firstName: 'John',
+      lastName: 'Smith',
+      age: 31,
+      address: {
+        street: '50 Main st',
+        city: 'Boston',
+        state: 'MA'
+      },
+      image: 'https://picsum.photos/600/600',
+      isActive: true,
+      balance: 100,
+      registered: new Date('01/02/2018 08:30:00')
+    }, {
+      firstName: 'Luke',
+      lastName: 'Doe',
+      age: 20,
+      address: {
+        street: '50 Main st',
+        city: 'Boston',
+        state: 'MA',
+      },
+      image: 'https://picsum.photos/600/600',
+      isActive: true,
+      balance: 500,
+      registered: new Date('01/05/2011 09:31:00')
+    }, {
+      firstName: 'John',
+      lastName: 'Doe',
+      age: 30,
+      address: {
+        street: '50 Main st',
+        city: 'Boston',
+        state: 'MA'
+      },
+      image: 'https://picsum.photos/600/600',
+      isActive: false,
+      balance: 50,
+      registered: new Date('01/02/2018 08:30:00')
+    }];
 
-  ngOnInit(): void {
   }
 
+  ngOnInit(): void {
+    this.setCurrentClasses();
+    this.setCurrentStyles();
+  }
+
+  private setCurrentClasses() {
+    this.currentClasses = {
+      'btn-success': this.isActive
+    }
+  }
+
+  setShowExtended(): void {
+    this.showExtended = !this.showExtended;
+    this.setCurrentStyles();
+  }
+
+  setCurrentStyles() {
+    this.currentStyles = {
+      'padding-top': this.showExtended ? '0' : '40px'
+    }
+  }
 }
